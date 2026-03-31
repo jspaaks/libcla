b40bd9432c225626f525b761e1d66f0cdbe1e275
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2026-03-31 12:46:17 -0400

    updated the example program

--- src/example/main.c
+++ src/example/main.c
@@ -3,70 +3,76 @@
 #include <stdlib.h>
 
 
-static const char * get_basename (const struct cla * cla);
-static const char * get_input_filename (const struct cla * cla);
-static size_t get_nsamples (const struct cla * cla);
-static const char * get_output_filename (const struct cla * cla);
-static int get_verbose (const struct cla * cla);
-
-
-static const char * get_basename (const struct cla * cla) {
-    const char * s = CLA_get_optional_value(cla, "--basename");
-    if (s == nullptr) {
-        return "sample.";
-    }
-    return s;
-}
-
-
-static const char * get_input_filename (const struct cla * cla) {
-    return CLA_get_positional_value(cla, 0);
-}
-
-
-static size_t get_nsamples (const struct cla * cla) {
-    const char * s = CLA_get_required_value(cla, "--nsamples");
-    size_t nsamples;
-    sscanf(s, " %zu", &nsamples);
-    return nsamples;
-}
-
-
-static const char * get_output_filename (const struct cla * cla) {
-    return CLA_get_positional_value(cla, 1);
-}
-
-
-static int get_verbose (const struct cla * cla) {
-    return CLA_count_flag(cla, "--verbose");
-}
-
-
 int main (int argc, const char * argv[]) {
     // create the kwargs object
     struct cla * cla = CLA_create();
 
-    // add various types of allowed arguments
-    CLA_add_required(cla, "--nsamples", "-s");
-    CLA_add_optional(cla, "--basename", "-b");
-    CLA_add_flag(cla, "--verbose", "-v");
+    // add a required argument with a longname and a shortname
+    CLA_add_required(cla, "--aa", "-a");
+
+    // add a required argument with a longname only
+    CLA_add_required(cla, "--bb", nullptr);
+
+    // add a required argument with a shortname only
+    CLA_add_required(cla, nullptr, "-c");
+
+    // add an optional argument with a longname and a shortname
+    CLA_add_optional(cla, "--dd", "-d");
+
+    // add an optional argument with a longname only
+    CLA_add_optional(cla, "--ee", nullptr);
+
+    // add an optional argument with a shortname only
+    CLA_add_optional(cla, nullptr, "-f");
+
+    // add a flag argument with a longname and a shortname
+    CLA_add_flag(cla, "--gg", "-g");
+
+    // add a flag argument with a longname only
+    CLA_add_flag(cla, "--hh", nullptr);
+
+    // add a flag argument with a shortname only
+    CLA_add_flag(cla, nullptr, "-i");
+
+    // specify that the program will have exactly 2 positional arguments
     CLA_add_positionals(cla, 2);
 
     // parse the arguments that the user actually chose to use
     CLA_parse(cla, argc, argv);
 
-    // retrieve values
-    const char * basename = get_basename(cla);
-    const size_t nsamples = get_nsamples(cla);
-    const int verbose_count = get_verbose(cla);
-    const char * input_filename = get_input_filename(cla);
-    const char * output_filename = get_output_filename(cla);
+    // retrieve value of required options
+    fprintf(stdout, "-a/--aa = %s\n", CLA_get_value_required(cla, "-a"));
+    fprintf(stdout, "--bb = %s\n", CLA_get_value_required(cla, "--bb"));
+    fprintf(stdout, "-c = %s\n", CLA_get_value_required(cla, "-c"));
 
-    fprintf(stdout, "basename = %s\n", basename);
-    fprintf(stdout, "nsamples = %zu\n", nsamples);
-    fprintf(stdout, "verbose used %d times\n", verbose_count);
-    fprintf(stdout, "input filename = %s\n", input_filename);
-    fprintf(stdout, "output filename = %s\n", output_filename);
+    // retrieve value of optional arguments
+    if (CLA_has_optional(cla, "-d")) {
+        fprintf(stdout, "-d/--dd = %s\n", CLA_get_value_optional(cla, "-d"));
+    }
+
+    if (CLA_has_optional(cla, "--ee")) {
+        fprintf(stdout, "--ee = %s\n", CLA_get_value_optional(cla, "--ee"));
+    }
+
+    if (CLA_has_optional(cla, "-f")) {
+        fprintf(stdout, "-f = %s\n", CLA_get_value_optional(cla, "-f"));
+    }
+
+    // check flags
+    fprintf(stdout, "-g/--gg = %s / %d times\n",
+            CLA_has_flag(cla, "-g") ? "used" : "not used",
+            CLA_count_flag(cla, "-g"));
+
+    fprintf(stdout, "--hh = %s / %d times\n",
+            CLA_has_flag(cla, "--hh") ? "used" : "not used",
+            CLA_count_flag(cla, "--hh"));
+
+    fprintf(stdout, "-i = %s / %d times\n",
+            CLA_has_flag(cla, "-i") ? "used" : "not used",
+            CLA_count_flag(cla, "-i"));
+
+    fprintf(stdout, "positional[0] = %s\n", CLA_get_value_positional(cla, 0));
+    fprintf(stdout, "positional[1] = %s\n", CLA_get_value_positional(cla, 1));
 
     // free memory resources
     CLA_destroy(&cla);
